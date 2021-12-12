89cc698b21e997170f6d6d07b45d3f44e08394b7
BeratErdinc <beraterdinc42@gmail.com>
2021-12-12 16:50:07 +0300

    C#BlackJack

---
+++ BlackJack(21)/BlackJack/BlackJack/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnbaşla = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.btnkartçek = new System.Windows.Forms.Button();
+            this.btndur = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kurpiyer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Oyuncu";
+            // 
+            // btnbaşla
+            // 
+            this.btnbaşla.Location = new System.Drawing.Point(30, 370);
+            this.btnbaşla.Name = "btnbaşla";
+            this.btnbaşla.Size = new System.Drawing.Size(89, 27);
+            this.btnbaşla.TabIndex = 2;
+            this.btnbaşla.Text = "Başla";
+            this.btnbaşla.UseVisualStyleBackColor = true;
+            this.btnbaşla.Click += new System.EventHandler(this.btnbaşla_Click);
+            // 
+            // btnreset
+            // 
+            this.btnreset.Location = new System.Drawing.Point(198, 370);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(87, 27);
+            this.btnreset.TabIndex = 3;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // btnkartçek
+            // 
+            this.btnkartçek.Location = new System.Drawing.Point(30, 290);
+            this.btnkartçek.Name = "btnkartçek";
+            this.btnkartçek.Size = new System.Drawing.Size(97, 24);
+            this.btnkartçek.TabIndex = 4;
+            this.btnkartçek.Text = "Kart Çek";
+            this.btnkartçek.UseVisualStyleBackColor = true;
+            this.btnkartçek.Click += new System.EventHandler(this.btnkartçek_Click);
+            // 
+            // btndur
+            // 
+            this.btndur.Location = new System.Drawing.Point(205, 290);
+            this.btndur.Name = "btndur";
+            this.btndur.Size = new System.Drawing.Size(80, 24);
+            this.btndur.TabIndex = 5;
+            this.btndur.Text = "Dur";
+            this.btndur.UseVisualStyleBackColor = true;
+            this.btndur.Click += new System.EventHandler(this.btndur_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(74, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 96);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(151, 137);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(71, 96);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::BlackJack.Properties.Resources.b1fv2;
+            this.pictureBox4.Location = new System.Drawing.Point(75, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(71, 96);
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(981, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btndur);
+            this.Controls.Add(this.btnkartçek);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.btnbaşla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Başla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnbaşla;
+        private System.Windows.Forms.Button btnreset;
+        private System.Windows.Forms.Button btnkartçek;
+        private System.Windows.Forms.Button btndur;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
